683b408c841621cb6b1ac2f96e04d6670fa1bbe8
Phill Kenoyer <phill@kenoyer.com>
2011-03-26 15:41:57 -0700

    Added serial number and id generator.

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,16 @@
 class Card
   include Mongoid::Document
+  include Mongoid::Timestamps
+
+  before_save :generate_id_from_vin
+
+  field :serial_number, :required => true
+  index :serial_number, :unique => true
+
+  private
+
+  def generate_id_from_serial_number
+    self.id = serial_number.gsub(/[^A-Z0-9]/,'').squeeze('').gsub(/^\-|\-$/,'')
+  end
+
 end
