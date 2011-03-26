1e8bfdd2aa4c450369f003392b3b04f393f25e9e
Phill Kenoyer <phill@kenoyer.com>
2011-03-25 19:50:23 -0700

    Initial commit of new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KindnessCard::Application.config.secret_token = '3c9f12fbf0af29ae7599ec3e894a38219b46ae6818aa4799a6152fa17bab0cf89a8b33d634759622c1316b56060ba274b7fb0dfd5b5a4d7f73f3210d5b554507'
