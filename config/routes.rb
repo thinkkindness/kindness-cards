2c765601560905ddf81596b35ef49be94f5e8def
Phill Kenoyer <phill@kenoyer.com>
2011-03-26 14:58:06 -0700

    Routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 KindnessCard::Application.routes.draw do
 
+  devise_for :accounts
+  resources :accounts, :only => :show
+
   resources :cards
 
   resources :tracks
 
-  devise_for :accounts
-  resources :accounts, :only => :show
   match '/:id' => 'cards#show'
   root :to => "home#index"
 
