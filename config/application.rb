e5479e33bcf78c1d51d4d3686eb2232912303b5c
David Davis <david.ddavis@gmail.com>
2011-03-27 16:25:08 -0700

    go back to test_unit default for testing framework generator
    cruft: rspec vestigials

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module KindnessCard
     config.generators do |g|
       g.orm             :active_record
       g.template_engine :haml
-      g.test_framework  :rspec, :fixture => true, :views => false
+      g.test_framework  :test_unit, :fixture => false, :views => false
       g.fixture_replacement :factory_girl, :dir => "spec/factories"
     end
   end
--- spec/models/learn_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Learn do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
