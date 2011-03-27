36679833bbe4e8d5b42d42bf9f564bd08a5f7791
Phill Kenoyer <phill@kenoyer.com>
2011-03-26 19:39:46 -0700

    HTML5

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,8 @@ require File.expand_path('../application', __FILE__)
 #require 'yaml'
 #YAML::ENGINE.yamler= 'syck'
 
-
 # Initialize the rails application
 KindnessCard::Application.initialize!
+
+# Force HTML5
+Haml::Template.options[:format] = :html5
