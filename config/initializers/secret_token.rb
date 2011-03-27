5cf3b85604ec448721b33b7ab3cdd605296e1a3b
El Skwid (Don Morrison) <elskwid@gmail.com>
2011-03-27 08:26:36 -0700

    Import freshie.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kindness::Application.config.secret_token = '9be57c6273a526618d27c039f9846d336dcd73390f0e3d588297477d0f2e6a3e031191e997a6714722a776e4c0ed89b1133a08145f156dd809c803e67a06bdb8'
