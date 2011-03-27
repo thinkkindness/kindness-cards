bc1439457b2301e71034572ee2e8da47eeb8aad6
Sterling Hamilton <sterling.hamilton+git-commit@gmail.com>
2011-03-26 20:18:52 -0700

    Fixing cruft uniqueness check.

--- app/models/account.rb
+++ app/models/account.rb
@@ -7,6 +7,5 @@ class Account
 
   field :name
   validates_presence_of :name
-  validates_uniqueness_of :name, :email, :case_sensitive => false
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me
 end
