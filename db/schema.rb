a063592c7ab33bedb4b70c5ad56e44eb756b447c
David Davis <david.ddavis@gmail.com>
2011-03-27 15:39:41 -0700

    note: be sure to commit db/schema after running a migration locally

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110327215609) do
+ActiveRecord::Schema.define(:version => 20110327220129) do
 
   create_table "accounts", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
