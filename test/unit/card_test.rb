3e5b8830dedea26be01e04f88f2b5ff4f67ac2bb
David Davis <david.ddavis@gmail.com>
2011-03-27 16:39:53 -0700

    more unit tests

--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -2,6 +2,8 @@ require 'test_helper'
 
 class CardTest < ActiveSupport::TestCase
 
+  REQUIRED_FIELDS = { :serial_number => 'foo' }
+
   def setup
     @card = Card.new
   end
@@ -9,4 +11,27 @@ class CardTest < ActiveSupport::TestCase
   test "serial_number is required" do
     assert_raise { @card.save! }
   end
+
+  test "uniqueness of serial number" do
+    Card.create!(:serial_number => 'foo')
+
+    assert_raise {
+      Card.create!(:serial_number => 'foo')
+    }
+  end
+
+  test "track creation" do
+    add_required_fields
+
+    @card.save!
+
+    @card.tracks.create!(TrackTest::REQUIRED_FIELDS)
+  end
+
+  private
+
+    def add_required_fields
+      @card.serial_number = 'foo'
+    end
+
 end
---
+++ test/unit/track_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TrackTest < ActiveSupport::TestCase
+
+  # simulate an object w/ valid fields
+  REQUIRED_FIELDS = { :location => "Reno, NV" }
+
+  def setup
+    @track = Track.new
+  end
+
+  test "requires card" do
+    assert_raise {
+      @track.save!
+    }
+  end
+
+  test "requires card (has card)" do
+    card = Card.create!(CardTest::REQUIRED_FIELDS)
+
+    card.tracks.create!(REQUIRED_FIELDS)
+  end
+
+end
