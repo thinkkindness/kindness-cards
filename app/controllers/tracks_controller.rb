5919958b5f953176ce4c3e720a67ec8922b3109e
Phill Kenoyer <phill@kenoyer.com>
2011-03-26 17:09:18 -0700

    s/xml/json

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,34 +1,34 @@
 class TracksController < ApplicationController
   # GET /tracks
-  # GET /tracks.xml
+  # GET /tracks.json
   def index
     @tracks = Track.all
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @tracks }
+      format.json  { render :json => @tracks }
     end
   end
 
   # GET /tracks/1
-  # GET /tracks/1.xml
+  # GET /tracks/1.json
   def show
     @track = Track.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @track }
+      format.json  { render :json => @track }
     end
   end
 
   # GET /tracks/new
-  # GET /tracks/new.xml
+  # GET /tracks/new.json
   def new
     @track = Track.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @track }
+      format.json  { render :json => @track }
     end
   end
 
@@ -38,46 +38,46 @@ class TracksController < ApplicationController
   end
 
   # POST /tracks
-  # POST /tracks.xml
+  # POST /tracks.json
   def create
     @track = Track.new(params[:track])
 
     respond_to do |format|
       if @track.save
         format.html { redirect_to(@track, :notice => 'Track was successfully created.') }
-        format.xml  { render :xml => @track, :status => :created, :location => @track }
+        format.json  { render :json => @track, :status => :created, :location => @track }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @track.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @track.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /tracks/1
-  # PUT /tracks/1.xml
+  # PUT /tracks/1.json
   def update
     @track = Track.find(params[:id])
 
     respond_to do |format|
       if @track.update_attributes(params[:track])
         format.html { redirect_to(@track, :notice => 'Track was successfully updated.') }
-        format.xml  { head :ok }
+        format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @track.errors, :status => :unprocessable_entity }
+        format.json  { render :json => @track.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   # DELETE /tracks/1
-  # DELETE /tracks/1.xml
+  # DELETE /tracks/1.json
   def destroy
     @track = Track.find(params[:id])
     @track.destroy
 
     respond_to do |format|
       format.html { redirect_to(tracks_url) }
-      format.xml  { head :ok }
+      format.json  { head :ok }
     end
   end
 end
