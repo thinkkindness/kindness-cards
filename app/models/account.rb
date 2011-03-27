b08fdf813ad04adee589c7c28e7340dfe5be7d44
David Davis <david.ddavis@gmail.com>
2011-03-27 14:01:12 -0700

    rails generate devise account
    migration: convert Devise to postgres

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,9 +1,11 @@
-class Account
-  include Mongoid::Document
+class Account < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
 
   field :name
   field :phone
