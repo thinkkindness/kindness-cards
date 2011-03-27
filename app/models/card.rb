9d1085c591888552cc9a78ea72381045e023522b
El Skwid (Don Morrison) <elskwid@gmail.com>
2011-03-27 13:50:38 -0700

    Add has_acts helper to card

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,7 @@
 class Card < ActiveRecord::Base
   has_many :acts
+  
+  def has_acts?
+    acts.present?
+  end
 end
--- test/unit/card_test.rb
+++ test/unit/card_test.rb
@@ -4,10 +4,16 @@ class CardTest < ActiveSupport::TestCase
   
   def setup
     @card = Factory(:card)
+    @card.acts << Factory(:act)
   end
   
   def test_card_uid
     assert_respond_to @card, :uid
+  end
+  
+  def test_has_acts_helper
+    assert_respond_to @card, :has_acts?
+    assert @card.has_acts?
   end
   
 end
