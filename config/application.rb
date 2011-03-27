5c9f67d06d9c4c8d171794cfa06818353589fad6
David Davis <david.ddavis@gmail.com>
2011-03-27 16:17:53 -0700

    still use factory_girl, but don't override :dir

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module KindnessCard
       g.orm             :active_record
       g.template_engine :haml
       g.test_framework  :test_unit, :fixture => false, :views => false
-      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.fixture_replacement :factory_girl
     end
   end
 end
