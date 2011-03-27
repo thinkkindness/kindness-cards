6bca5e49fe1c4e8944ffc664b135ab3aaed9c4f0
David Davis <david.ddavis@gmail.com>
2011-03-26 21:14:28 -0700

    cruft: remove scaffold comments

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,44 +1,35 @@
 class AccountsController < ApplicationController
-  # GET /accounts
-  # GET /accounts.xml
   def index
     @accounts = Account.all
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.xml  { render :xml => @accounts }
     end
   end
 
-  # GET /accounts/1
-  # GET /accounts/1.xml
   def show
     @account = Account.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.xml  { render :xml => @account }
     end
   end
 
-  # GET /accounts/new
-  # GET /accounts/new.xml
   def new
     @account = Account.new
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html
       format.xml  { render :xml => @account }
     end
   end
 
-  # GET /accounts/1/edit
   def edit
     @account = Account.find(params[:id])
   end
 
-  # POST /accounts
-  # POST /accounts.xml
   def create
     @account = Account.new(params[:account])
 
@@ -53,8 +44,6 @@ class AccountsController < ApplicationController
     end
   end
 
-  # PUT /accounts/1
-  # PUT /accounts/1.xml
   def update
     @account = Account.find(params[:id])
 
@@ -69,8 +58,6 @@ class AccountsController < ApplicationController
     end
   end
 
-  # DELETE /accounts/1
-  # DELETE /accounts/1.xml
   def destroy
     @account = Account.find(params[:id])
     @account.destroy
