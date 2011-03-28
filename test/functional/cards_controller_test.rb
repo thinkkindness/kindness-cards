1f274f35152606c2d774a2feb92279a225eec835
David Davis <david.ddavis@gmail.com>
2011-03-27 17:46:00 -0700

    more tests: CardsController

--- test/functional/cards_controller_test.rb
+++ test/functional/cards_controller_test.rb
@@ -2,7 +2,11 @@ require File.expand_path '../test_helper', File.dirname(__FILE__)
 
 class CardsControllerTest < ActionController::TestCase
 
-  test "cards" do
+  test "cards#show routing" do
+    assert_routing '/cards/xxx', { :controller => 'cards', :action => 'show', :id => 'xxx' }
+  end
+
+  test "cards#new routing" do
     assert_routing '/cards/new', { :controller => 'cards', :action => 'new' }
   end
 
