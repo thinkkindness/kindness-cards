dec868b806836632c413285aeaf303ed242c3f50
Phill Kenoyer <phill@kenoyer.com>
2011-03-28 18:45:38 -0700

    Added override for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  # Redirect root if not authorized
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
+  end
+
+  # override current_user to current_account
+  def current_ability
+    @current_ability ||= Ability.new(current_account)
   end
 
 end
