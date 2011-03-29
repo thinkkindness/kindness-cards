49ccc1bf412601981f1c1eea147eb4ddf9ce120b
Phill Kenoyer <phill@kenoyer.com>
2011-03-28 18:59:03 -0700

    removed bad migration

--- db/migrate/20110327225848_accounts_cards_associations.rb
+++
@@ -1,9 +0,0 @@
-class AccountsCardsAssociations < ActiveRecord::Migration
-  def self.up
-    add_column :cards, :account_id, :integer
-  end
-
-  def self.down
-    remove_column :cards, :account_id
-  end
-end
