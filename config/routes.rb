cb97717302801eaa607216288a89dabc3bdd42cb
Phill Kenoyer <phill@kenoyer.com>
2011-10-06 22:43:47 -0700

    changed routes so users belong to accounts

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,34 @@
 KindnessCard::Application.routes.draw do
 
   devise_for :users
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+    get "/register" => "devise/registrations#new"
+    get "/reset" => "devise/passwords#new"
+  end
 
-  resources :users, :shallow => true do
-    resources :accounts do
-      resources :cards do
-        resources :tracks
-      end
+  resources :accounts do
+    resources :users
+    resources :cards do
+      resources :tracks
     end
   end
+
+  resources :users
+  resources :accounts
 
   resources :cards do
     resources :tracks
   end
+
   resources :tracks
 
   match '/learn' => 'pages#learn'
   match '/home' => 'pages#home'
 
-  match '/:serial_number' => 'tracks#new'
-  match '/:serial_number/messages' => 'messages#new'
+  # match '/:serial_number' => 'tracks#new'
+  # match '/:serial_number/messages' => 'messages#new'
 
   root :to => "tracks#new"
 
