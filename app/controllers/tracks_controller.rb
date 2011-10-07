113aaa3ffe7c9551ac6ca8508435882add36fd9a
David Davis <duonoid@gmail.com>
2011-10-06 18:44:38 -0700

    WIP: remove direct access to instance variables (tracks)
    using decent_exposure

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,87 +1,71 @@
 class TracksController < ApplicationController
 
-  def index
-    @tracks = Track.all
+  expose(:card) do
+    track.card ||
+        Card.find_by_serial_number(params[:serial_number] || params[:card_id]) ||
+        Card.find(params[:card_id]) ||
+        Card.find(params[:track][:serial_number])
+  end
+  expose(:tracks) { Track.all }
+  expose(:track)
+    # TODO: a Track has an owner once created, and should only be editable by them
 
+  def index
     respond_to do |format|
       format.html
-      format.json  { render :json => @tracks }
+      format.json  { render :json => tracks }
     end
   end
 
   def show
-    @track = Track.find(params[:id])
-    @card = Card.find(@track.card_id)
-
     respond_to do |format|
       format.html
-      format.json  { render :json => @track }
+      format.json  { render :json => track }
     end
   end
 
   def new
-    if params[:serial_number]
-      @card = Card.find_by_serial_number(params[:serial_number])
-    elsif params[:card_id]
-      unless @card = Card.find_by_serial_number(params[:card_id])
-        @card = Card.find(params[:card_id]) if params[:card_id].to_i != 0
-      end
-    end
-    @track = Track.new
-
     respond_to do |format|
       format.html
-      format.json  { render :json => @track }
+      format.json  { render :json => track }
     end
   end
 
   def create
-    if params[:card_id]
-      @card = Card.find(params[:card_id])
-    elsif params[:track][:serial_number]
-      @card = Card.find_by_serial_number(params[:track][:serial_number])
-    end
-    @track = @card.tracks.build(params[:track])
-
     respond_to do |format|
-      if @track.save
-        format.html { redirect_to edit_track_path(@track) }
-        format.json  { render :json => @track, :status => :created, :location => @track }
+      if track.save
+        format.html { redirect_to edit_track_path(track) }
+        format.json  { render :json => track, :status => :created, :location => track }
       else
         format.html { render :action => "new", :flash => "Didn't save!" }
-        format.json  { render :json => @track.errors, :status => :unprocessable_entity }
+        format.json  { render :json => track.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def edit
-    @track = Track.find(params[:id])
-    @card = @track.card
   end
 
   def update
-    # TODO: a Track has an owner by now, and should only be editable by them
-    @track = Track.find(params[:id])
-    @card = @track.card
-
     respond_to do |format|
-      if @track.update_attributes(params[:track])
-        format.html { redirect_to(@card, :notice => 'Track was successfully updated.') }
+      if track.update_attributes(params[:track])
+        format.html { redirect_to(card, :notice => 'Track was successfully updated.') }
         format.json  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.json  { render :json => @track.errors, :status => :unprocessable_entity }
+        format.json  { render :json => track.errors, :status => :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @track = Track.find(params[:id])
-    @track.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(tracks_url) }
-      format.json  { head :ok }
+    if track.destroy
+      respond_to do |format|
+        format.html { redirect_to(tracks_url) }
+        format.json  { head :ok }
+      end
+    else
+      # FAIL
     end
   end
 end
