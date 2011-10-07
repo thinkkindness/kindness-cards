0b360792ee8a42b86fe18c52590b3daa3fb57a87
David Davis <duonoid@gmail.com>
2011-10-06 18:20:20 -0700

    cleanup: remove default message override

--- app/models/card.rb
+++ app/models/card.rb
@@ -13,7 +13,7 @@ class Card < ActiveRecord::Base
 
   validates_uniqueness_of :serial_number
 
-  validates_presence_of :serial_number, :message => "can't be blank"
+  validates_presence_of :serial_number
 
   belongs_to :accounts
   has_many :tracks
